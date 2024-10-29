a1eedfabb997bd1f8b7c458d70d5f2e8aa596aa5
gyeongmann <hkc8857@naver.com>
2024-10-29 23:41:27 +0900

    Update 경찬.java

--- week14/불량 사용자/경찬.java
+++ week14/불량 사용자/경찬.java
@@ -1,5 +1,86 @@
 /*
 정확성  테스트
+테스트 1 〉	통과 (0.04ms, 72.2MB)
+테스트 2 〉	통과 (0.09ms, 75.8MB)
+테스트 3 〉	통과 (0.10ms, 69.1MB)
+테스트 4 〉	통과 (0.09ms, 76.9MB)
+테스트 5 〉	통과 (11.15ms, 76.8MB)
+테스트 6 〉	통과 (1.13ms, 91MB)
+테스트 7 〉	통과 (0.06ms, 78.9MB)
+테스트 8 〉	통과 (0.06ms, 84.8MB)
+테스트 9 〉	통과 (0.07ms, 78.3MB)
+테스트 10 〉	통과 (0.06ms, 72.1MB)
+테스트 11 〉	통과 (0.13ms, 82MB)
+*/
+
+import java.util.*;
+
+class Solution {
+    int cnt = 0;
+    boolean[] visited;
+    Set<Integer> tmp = new HashSet<>();
+    public int solution(String[] user_id, String[] banned_id) {
+        int answer = 0;
+        
+        int r = user_id.length;
+        int c = banned_id.length;
+        visited = new boolean[r];
+        int[][] map = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (compare(user_id[i], banned_id[j])) {
+                    map[i][j] = 1;
+                }
+            }
+        }
+        
+        // for (int[] row : map) {
+        //     System.out.println(Arrays.toString(row));
+        // }
+        // System.out.println();
+        
+        dfs(0, 0, map);
+        // for (Set<Integer> row : tmp) {
+        //     System.out.println(row);
+        // }
+        answer = tmp.size();
+        return answer;
+    }
+    
+    void dfs(int banIdx, int bit, int[][] map) {
+        if (banIdx == map[0].length) {
+            tmp.add(bit);
+            return;
+        }
+        
+        for (int i = 0; i < map.length; i++) {
+            int curr = 1 << i;
+            if ((bit & curr) > 0) continue;
+            
+            if (map[i][banIdx] == 1) {
+                bit += curr;
+                dfs(banIdx+1, bit, map);
+                bit -= curr;
+            }
+        }
+    }
+    
+    boolean compare(String user_id, String banned_id) {
+        if (user_id.length() != banned_id.length()) return false;
+        
+        for (int i = 0; i < user_id.length(); i++) {
+            char ch1 = user_id.charAt(i);
+            char ch2 = banned_id.charAt(i);
+            if (ch2 == '*') continue;
+            if (ch1 != ch2) return false;
+        }
+        return true;
+    }
+}
+
+// ----------------------------------------------------
+/*
+정확성  테스트
 테스트 1 〉	통과 (0.10ms, 76.9MB)
 테스트 2 〉	통과 (0.37ms, 72MB)
 테스트 3 〉	통과 (0.29ms, 73.9MB)
